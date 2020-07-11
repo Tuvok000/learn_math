def5ec2e078c278db98db47c60f0a30d35d33f5f
user <user@kali>
2020-07-10 22:09:25 -0600

    added initial reference lookup

--- part1.c
+++ part1.c
@@ -22,9 +22,42 @@ void rand_gen(int *var, int min, int max)
 
 int main()
 {
+  printf("Welcome to Learn Math Alpha V0.0.1");
+  printf("Select difficulty\n");
+  
+  //later on, expand this section to automatically space values instead of hardcoded strings
+  printf("| Difficulty | Operators | Value From | Value To |\n");
+  printf("+------------+-----------+------------+----------+\n");
+  printf("| 1          | +         | 0          | 10       |\n");
+  printf("| 2          | +         | 0          | 25       |\n");
+  printf("| 3          | +         | 0          | 50       |\n");
+  printf("| 4          | +         | 0          | 100      |\n");
+  printf("| 5          | +         | 0          | 250      |\n");
+  printf("+------------+-----------+------------+----------+\n");
+  printf("| 6          | -         | 0          | 10       |\n");
+  printf("| 7          | -         | 0          | 25       |\n");
+  printf("| 8          | -         | 0          | 50       |\n");
+  printf("| 9          | -         | 0          | 100      |\n");
+  printf("| 10         | -         | 0          | 250      |\n");
+  printf("+------------+-----------+------------+----------+\n");
+  printf("| 11         | +/-       | -10        | 10       |\n");
+  printf("| 12         | +/-       | -25        | 20       |\n");
+  printf("| 13         | +/-       | -50        | 50       |\n");
+  printf("| 14         | +/-       | -100       | 100      |\n");
+  printf("| 15         | +/-       | -250       | 250      |\n");
+  printf("+------------+-----------+------------+----------+\n");
+
+  //reference array for above diagram
+  int reference[15][3] = {{0, 0, 10}, {0, 0, 25}, {0, 0, 50}, {0, 0, 100}, {0, 0, 250},
+			  {1, 0, 10}, {1, 0, 25}, {1, 0, 50}, {1, 0, 100}, {1, 0, 250},
+			  {2, -10, 10}, {2, -25, 25}, {2, -50, 50}, {2, -100, 100}, {2, -250, 250}};
+  unsigned int level;
+  level = getc();
+  printf("%d, %d, %d, %d\n", level, reference[level][0], reference[level][1], reference[level][2]);
+  /*
   time_t t;
   int i, j, val[2];
-  int operator; //designates whether addition or subtraction is performed
+  int operator; //0 - add, 1 - minus, 2 - add/minus (rand)
   srand(time(0));
   FILE *output, *answers;
   output = fopen("output.txt", "w");
@@ -48,5 +81,6 @@ fprintf(answers, "%2d)%4d\t", i*2+1, val[0] + val[1]);
   //close both work file and solution file
   fclose(output);
   fclose(answers);
+  */
   return 0;
 }
