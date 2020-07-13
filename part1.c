c3664669a3e4b4be2fb1d1008d0ba3a33f871fb5
user <user@kali>
2020-07-12 23:07:47 -0600

    asdf

--- part1.c
+++ part1.c
@@ -35,13 +35,13 @@ int num_input(int length)
   int i;//generic variable for use in for loops
   unsigned int input = 0;//stores input from getchar()
   int count = 0; //keeps track of values entered to later multiply my powers of 10
+  int output = 0;
   
   for(i = 0; i < length; i++)
     {
       while(1)
 	{
-	  input = getchar();
-	  //user entered a value between 0-9
+	  input = getc(stdin);
 	  if (input >= '0' && input <= '9')
 	    {
 	      digit[i] = input - '0';//convert ascii number value to actual number value
@@ -49,12 +49,12 @@ int num_input(int length)
 	      break;
 	    }
 	  //user presed enter without entering at least one number
-	  else if (input == '\r' && i == 0)
+	  else if ((input == '\r' || input == '\n') && i == 0)
 	    {
 	      printf("Must enter at least one value\n");
 	    }
 	  //user pressed enter with at least one number entered
-	  else if (input == '\r' && i > 0)
+	  else if ((input == '\r' || input == '\n') && count > 0)
 	    {
 	      i = length;
 	      break;
@@ -69,15 +69,17 @@ int num_input(int length)
 
   //multiplies the individual digits to create a single number
   //ex: x*10^2 + y*10^1 + z*10^0
-  for(i = count; i >= 0; i--)
+  for(i = 0; i < count; i++)
     {
-      
-	  
+      output += digit[i] * pow(10, count - i - 1);
+    }
+  return output;
+}
         
 
 int main()
 {
-  printf("Welcome to Learn Math Alpha V0.0.1");
+  printf("Welcome to Learn Math Alpha V0.0.1\n");
   printf("Select difficulty\n");
   
   //later on, expand this section to automatically space values instead of hardcoded strings
@@ -105,12 +107,14 @@ int main()
   //reference array for above diagram
   int reference[15][3] = {{0, 0, 10}, {0, 0, 25}, {0, 0, 50}, {0, 0, 100}, {0, 0, 250},
 			  {1, 0, 10}, {1, 0, 25}, {1, 0, 50}, {1, 0, 100}, {1, 0, 250},
-			  {2, -10, 10}, {2, -25, 25}, {2, -50, 50}, {2, -100, 100}, {2, -250, 250}};
+			  {0, -10, 10}, {0, -25, 25}, {0, -50, 50}, {0, -100, 100}, {0, -250, 250}};
   unsigned int level;
   //function to input and validate character input
-  level = getchar();
-  printf("%d, %d, %d, %d\n", level, reference[level][0], reference[level][1], reference[level][2]);
-  /*
+  printf("Select a level\n-> ");
+  level = num_input(2);
+  level--;
+  //printf("%d, %d, %d, %d\n", level, reference[level][0], reference[level][1], reference[level][2]);
+  
   time_t t;
   int i, j, val[2];
   int operator; //0 - add, 1 - minus, 2 - add/minus (rand)
@@ -123,20 +127,22 @@ int main()
   for(i = 0; i < 20; i++)
     {
 	//add check for second value to modify the sign
-      for(j = 0; j < 2; j++) rand_gen(val[j], -9, 9);
+      for(j = 0; j < 2; j++) rand_gen(&val[j], reference[level][1], reference[level][2]);
       if(val[1] >= 0)
       {
-	fprintf(output, "%2d) %d + %d = \t\t", i*2+1, val[0], val[1]);
+	fprintf(output, "%2d) %d + %d = \t\t\n", i + 1, val[0], val[1]);
       }
       else
       {
-  	fprintf(output, "%2d) %d -
-fprintf(answers, "%2d)%4d\t", i*2+1, val[0] + val[1]);
+  	fprintf(output, "%2d) %d - %d = \t\t\n", i + 1, val[0], abs(val[1]));
+      }
+      
+      fprintf(answers, "%2d)%4d\t\n", i + 1, val[0] + val[1]);
     }
 
   //close both work file and solution file
   fclose(output);
   fclose(answers);
-  */
+
   return 0;
 }
