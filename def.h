b6cb88df89af4375c804074df506fae14afbeb06
user <timkarlow@gmail.com>
2021-02-25 10:37:47 -0700

    asdf

--- def.h
+++ def.h
@@ -1,7 +1,8 @@
 #ifndef DEF_H
 #define DEF_H
 
-char category[][15] = {"Addition", "Subtraction", "Multiplication", "Division"};
+char category[][17] = {"Addition", "Subtraction", "Add + Sub Mixed",
+		       "Multiplication", "Division", "Mult + Div Mixed"};
 
 
 
--- main.c
+++ main.c
@@ -2,24 +2,49 @@
 #include <stdlib.h>
 #include "def.h"
 
+#define CAT_RANGE_HIGH 6
+#define CAT_RANGE_LOW 1
+
+//category definitions
+
+void addition(int level);
+/***************************************
+first category, addition
+adding 2-4 numbers together
+each number 1-4 digits long
+
+variable: level
+1 - 2 num, 1 digit
+
+***************************************/
 
 int main()
 {
-  int i;
-
+  int category_count;
+  long category_select;
+  char input[2];
   
   printf("Welcome to Learn Math Alpha V0.0.1\n");
   printf("Select module\n");
 
   //print categories defined in def.h
-  for(i = 0; i < 4; i++)
+  for(category_count = 0; category_count < 6; category_count++)
     {
-      printf("%d - %s\n", i+1, category[i]);
+      printf("%d - %s\n", category_count+1, category[category_count]);
     }
 
   //wait for user to select a category
-  printf("> ");
-  getchar("
+  while(1)
+    {
+      printf("> ");
+      scanf("%2s", input);
+      fflush(stdin);
+      category_select = strtol(input, NULL, 10);
+      if(category_select < CAT_RANGE_LOW || category_select > CAT_RANGE_HIGH)
+	continue;
+      break;
+    }
+  printf("done\n");
   
   return 0;
 }
